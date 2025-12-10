f76315c4e053f1ec04566b00ac5b0f6062eed75b
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-12-10 14:30:25 -0500

    * updated the CategoryController to match the ProductController but fitted for Categories

--- workbook-9/NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/controllers/CategoriesController.java
+++ workbook-9/NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/controllers/CategoriesController.java
@@ -1,74 +1,33 @@
 package com.pluralsight.NorthwindTradersAPI.controllers;
 
+import com.pluralsight.NorthwindTradersAPI.interfaces.CategoryDAO;
 import com.pluralsight.NorthwindTradersAPI.models.Category;
-import com.pluralsight.NorthwindTradersAPI.models.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class CategoriesController {
-    private DataSource dataSource;
+    private CategoryDAO categoryDAO;
 
     @Autowired
-    public CategoriesController(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public CategoriesController(CategoryDAO categoryDAO) {
+        this.categoryDAO = categoryDAO;
     }
 
     @RequestMapping(path = "/categories", method = RequestMethod.GET)
     public List<Category> getAllCategories() {
-        List<Category> categories = new ArrayList<>();
-
-        String query = "SELECT  CategoryID, CategoryName " +
-                "FROM categories " +
-                "ORDER BY CategoryID;";
-
-        try (Connection connection = dataSource.getConnection()) {
-            PreparedStatement statement = connection.prepareStatement(query);
-            ResultSet rows = statement.executeQuery();
-
-            while (rows.next()) {
-                categories.add(new Category(rows.getInt(1), rows.getString(2)));
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        List<Category> categories = categoryDAO.getAllCategories();
         return categories;
     }
 
     @RequestMapping(path = "/categories/{categoryID}", method = RequestMethod.GET)
     public List<Category> getCategoryByID(@PathVariable int categoryID) {
-        List<Category> categories = new ArrayList<>();
-
-        String query = "SELECT  CategoryID, CategoryName " +
-                "FROM categories " +
-                "WHERE CategoryID = ?;";
-
-        try {
-            Connection connection = dataSource.getConnection();
-
-            try (PreparedStatement statement = connection.prepareStatement(query)) {
-                statement.setInt(1, categoryID);
-
-                try (ResultSet rows = statement.executeQuery()) {
-                    while (rows.next()) {
-                        categories.add(new Category(rows.getInt(1), rows.getString(2)));
-                    }
-                }
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        List<Category> categories = categoryDAO.getCategoryByID(categoryID);
         return categories;
     }
 }
