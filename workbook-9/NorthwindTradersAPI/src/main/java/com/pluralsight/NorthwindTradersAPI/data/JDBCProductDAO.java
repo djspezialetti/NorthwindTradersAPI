74e94686e195aa214c4f9376d74e2cc500b5de98
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-12-10 16:18:31 -0500

    * added TO-DOs * updated the addProduct method to properly add products to the database * working on updating other queries to not utilize a JOIN when searching for that data

--- workbook-9/NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/data/JDBCProductDAO.java
+++ workbook-9/NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/data/JDBCProductDAO.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,6 +25,8 @@ public class JDBCProductDAO implements ProductDAO {
     public List<Product> getAllProducts() {
         List<Product> products = new ArrayList<>();
 
+        // TO-DO: Update query to not include the JOIN
+        // TO-DO: Update products to take in a CategoryID as an int
         String query = "SELECT P.ProductID, P.ProductName, C.CategoryName, P.UnitPrice " +
                 "FROM products AS P " +
                 "JOIN categories AS C ON (C.CategoryID = P.CategoryID) " +
@@ -51,10 +50,10 @@ public class JDBCProductDAO implements ProductDAO {
     public List<Product> getProductByID(int id) {
         List<Product> products = new ArrayList<>();
 
-        String query = "SELECT P.ProductID, P.ProductName, C.CategoryName, P.UnitPrice " +
-                "FROM products AS P " +
-                "JOIN categories AS C ON (C.CategoryID = P.CategoryID) " +
-                "WHERE P.ProductID = ?;";
+        // TO-DO: Update products to take in a CategoryID as an int
+        String query = "SELECT ProductID, ProductName, CategoryID, UnitPrice " +
+                "FROM products " +
+                "WHERE ProductID = ?;";
         try {
             Connection connection = dataSource.getConnection();
 
@@ -72,5 +71,34 @@ public class JDBCProductDAO implements ProductDAO {
             throw new RuntimeException(e);
         }
         return products;
+    }
+
+    @Override
+    public Product addProduct(Product product){
+        String query = "INSERT INTO products (ProductName, CategoryID, UnitPrice) " +
+                "VALUES (?, ?, ?);";
+
+        try {
+            Connection connection = dataSource.getConnection();
+
+            try (PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+                statement.setString(1, product.getName());
+                statement.setString(2, product.getCategory());
+                statement.setDouble(3, product.getPrice());
+
+                int rowsAffected = statement.executeUpdate();
+
+                ResultSet rs = statement.getGeneratedKeys();
+
+                if (rs.next()) product.setProductID(rs.getInt(1));
+
+                if (rowsAffected > 0) {
+                    System.out.println("Product added successfully!");
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return product;
     }
 }
