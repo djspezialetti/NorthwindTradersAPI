e4a9e9ef9372870e3708fb33e5228c1272311ef0
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-12-10 11:23:25 -0500

    * added model data for Category

--- workbook-9/NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/models/Category.java
+++ workbook-9/NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/models/Category.java
@@ -1,4 +1,32 @@
 package com.pluralsight.NorthwindTradersAPI.models;
 
 public class Category {
+    private int categoryID;
+    private String categoryName;
+
+    public Category(int categoryID, String categoryName) {
+        this.categoryID = categoryID;
+        this.categoryName = categoryName;
+    }
+
+    public int getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(int categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    @Override
+    public String toString() {
+        return "CategoryID: " + categoryID + "\tCategoryName: " + categoryName;
+    }
 }
