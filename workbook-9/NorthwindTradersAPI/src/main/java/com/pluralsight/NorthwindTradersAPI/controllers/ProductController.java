1990a2d28920162edee714d78fd83525e7d8a44d
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-12-10 12:01:16 -0500

    * working on adding code to allow the user to search for a specific Product by inputting the ID into the URL

--- workbook-9/NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/controllers/ProductController.java
+++ workbook-9/NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/controllers/ProductController.java
@@ -51,6 +51,22 @@ public class ProductController {
 //    public List<Product> getProductByID(@RequestParam(required = false) int productID){
 //        List<Product> products = new ArrayList<>();
 //
+//        String query = "SELECT P.ProductID, P.ProductName, C.CategoryName, P.UnitPrice " +
+//                "FROM products AS P " +
+//                "JOIN categories AS C ON (C.CategoryID = P.CategoryID) " +
+//                "ORDER BY P.ProductID;";
+//        try (Connection connection = dataSource.getConnection()){
+//            PreparedStatement statement = connection.prepareStatement(query);
+//            ResultSet rows = statement.executeQuery();
+//
+//            while (rows.next()) {
+//                products.add(new Product(rows.getInt(1), rows.getString(2),
+//                        rows.getString(3), rows.getDouble(4)));
+//            }
+//
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
 //
 //        return products;
 //    }
