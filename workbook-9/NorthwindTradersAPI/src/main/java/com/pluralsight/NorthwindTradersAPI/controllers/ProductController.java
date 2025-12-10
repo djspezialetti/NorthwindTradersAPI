4cd5d467b8970a17c1b38334f5be07d6265d3653
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-12-10 11:48:09 -0500

    * added an Autowired bean for Spring to allow the program to connect to the database within the ProductController class * imported sql frameworks so the ProductController class can properly connect and query the database

--- workbook-9/NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/controllers/ProductController.java
+++ workbook-9/NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/controllers/ProductController.java
@@ -1,21 +1,33 @@
 package com.pluralsight.NorthwindTradersAPI.controllers;
 
 import com.pluralsight.NorthwindTradersAPI.models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class ProductController {
+    private DataSource dataSource;
+
+    @Autowired
+    public ProductController(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
 
     @RequestMapping(path="/products", method = RequestMethod.GET)
     public List<Product> getAllProducts(){
-
         List<Product> products = new ArrayList<>();
+
         String query = "SELECT P.ProductID, P.ProductName, C.CategoryName, P.UnitPrice " +
                 "FROM products AS P " +
                 "JOIN categories AS C ON (C.CategoryID = P.CategoryID) " +
@@ -25,14 +37,13 @@ public class ProductController {
             ResultSet rows = statement.executeQuery();
 
             while (rows.next()) {
-                this.products.add(new Product(rows.getInt(1), rows.getString(2),
+                products.add(new Product(rows.getInt(1), rows.getString(2),
                         rows.getString(3), rows.getDouble(4)));
             }
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-
         return products;
     }
 
